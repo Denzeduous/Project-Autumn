560e1dcb559736fcb7e9aec1123b93c4678a734f
F0x1fy <alecmaxsanchez@gmail.com>
2019-11-28 13:08:22 -0700

    Fixed name. Juniper was the old project name when I first wanted to make this. Ignore it.

--- credentials.py
+++ credentials.py
@@ -1,2 +1,2 @@
-JuniperBot = {"user": "user", "password": "password", "database": "JuniperBot", "host": "127.0.0.1"}
+AutumnBot = {"user": "user", "password": "password", "database": "AutumnBot", "host": "127.0.0.1"}
 Sessions = {"user": "user", "password": "password", "database": "Sessions", "host": "127.0.0.1"}
