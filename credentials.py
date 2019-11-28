b7c07d1126d320760e457d7c504a8294962e721e
F0x1fy <alecmaxsanchez@gmail.com>
2019-11-28 12:43:54 -0700

    Figured out how I wanted to set up the credentials by default.

--- credentials.py
+++ credentials.py
@@ -1,1 +1,2 @@
-credentials = {"user": "user", "password": "password", "database": "db", "host": "127.0.0.1"}
+JuniperBot = {"user": "user", "password": "password", "database": "JuniperBot", "host": "127.0.0.1"}
+Sessions = {"user": "user", "password": "password", "database": "Sessions", "host": "127.0.0.1"}
